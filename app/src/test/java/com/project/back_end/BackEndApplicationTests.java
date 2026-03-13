c79f4e3eeeef49a6d920ad86ba568635d1444279
samuel <samuelcamarenacaballer@gmail.com>
2026-03-13 13:51:36 +0100

    feat(properties): new test for validation regex are read and available
    Test Jakarta validation regex configured at application properties.

--- app/src/test/java/com/project/back_end/BackEndApplicationTests.java
+++ app/src/test/java/com/project/back_end/BackEndApplicationTests.java
@@ -1,13 +1,26 @@
 package com.project.back_end;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BackEndApplicationTests {
-
+    
+    @Value("${email-validation-regex}")
+    private String emailValidationRegex;
+    
 	@Test
 	void contextLoads() {
 	}
+    
+    @Test
+    void givenEmailValidationPropertyExists_whenItIsInjected_thenPropertyIsAvailable() throws Exception {
+        assertThat(emailValidationRegex)
+            .isEqualTo("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$");
+    }
 
 }
