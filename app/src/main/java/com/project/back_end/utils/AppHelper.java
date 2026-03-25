c7c46e89c0fccfe588f73f90aaf35ede168b26a2
samuel <samuelcamarenacaballer@gmail.com>
2026-03-25 16:21:28 +0100

    refactor(utils): inject Application properties as String constant values
    Change String literal values of email and password validation Regex, to same values but inject from application.properties file.

--- app/src/main/java/com/project/back_end/utils/AppHelper.java
+++ app/src/main/java/com/project/back_end/utils/AppHelper.java
@@ -2,18 +2,21 @@ package com.project.back_end.utils;
 
 import org.springframework.beans.factory.annotation.Value;
 
-public class ApplicationHelper {
+public class AppHelper {
     
-    //@Value("${email-validation-regex}")
+    @Value("${email-validation-regex}")
     public static final String EMAIL_VALIDATION_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$";
     
-    // OWASP Simple Password Validation Regex
-    // - 4 to 8 character password requiring numbers and both lowercase and uppercase letters
-    //@Value("${password-simple-validation-regex}")
+    /*
+      OWASP Simple Password Validation Regex
+      - 4 to 8 character password requiring numbers and both lowercase and uppercase letters
+     */
+    @Value("${password-simple-validation-regex}")
     public static final String PASSWORD_SIMPLE_VALIDATION_REGEX = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$";
     
-    // OWASP Complex Password Validation Regex
-    // - 4 to 8 character password requiring numbers and both lowercase and uppercase letters
+    /*
+      OWASP Complex Password Validation Regex
+     */
     //@Value("${password-complex-validation-regex}")
     public static final String PASSWORD_COMPLEX_VALIDATION_REGEX
         = "^(?:(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])|(?=.*\\d)(?=.*[^A-Za-z0-9])(?=.*[a-z])|(?=.*[^A-Za-z0-9])" +
