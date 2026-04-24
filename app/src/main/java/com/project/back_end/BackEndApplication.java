cc74ce3df65452888aa101c7e7639273fdbf2a74
samuel <samuelcamarenacaballer@gmail.com>
2026-04-24 21:12:54 +0200

    refactor(main): change minor syntax details

--- app/src/main/java/com/project/back_end/BackEndApplication.java
+++ app/src/main/java/com/project/back_end/BackEndApplication.java
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.project.back_end")
 public class BackEndApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(BackEndApplication.class, args);
-	}
-
+        SpringApplication.run(BackEndApplication.class, args);
+    }
 }
