ae49e0e85e421064a6e8e34e97f38055ba986cf8
samuel <samuelcamarenacaballer@gmail.com>
2026-04-29 21:03:55 +0200

    feat(exception): add Jakarta Constraint Violation handling method to Global Exception Handler

--- app/src/main/java/com/project/back_end/controllers/ValidationFailed.java
+++ app/src/main/java/com/project/back_end/controllers/ValidationFailed.java
@@ -1,5 +1,9 @@
 package com.project.back_end.controllers;
 
+import com.project.back_end.utils.outputhelpers.MessageFormatter.MessageHead;
+import jakarta.validation.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,19 +14,27 @@ import org.springframework.validation.FieldError;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.project.back_end.utils.AppHelper.composeResponse;
+
 @RestControllerAdvice
 public class ValidationFailed {
-
+    private static final Logger logger = LoggerFactory.getLogger(ValidationFailed.class);
+    
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-        
         // Iterate through all the validation errors
         for (FieldError error : ex.getBindingResult().getFieldErrors()) {
             String errorMessage = error.getDefaultMessage();
             errors.put("message", "" + errorMessage);
         }
-
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+    
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handleConstraintViolation(ConstraintViolationException ex) {
+        String msg = ex.getConstraintViolations().iterator().next().getMessage();
+        logger.error("{}handleConstraintViolation:: {}", MessageHead.ERROR.compose(), msg);
+        return composeResponse(HttpStatus.BAD_REQUEST, "error", msg);
     }
 }
