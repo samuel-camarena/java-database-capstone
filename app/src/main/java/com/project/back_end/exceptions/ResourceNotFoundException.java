939e7709c4b329f6011723a315f7a0f74f2947bc
samuel <samuelcamarenacaballer@gmail.com>
2026-04-29 20:59:30 +0200

    feat(exception): add full implementation for failing while searching for an entity

---
+++ app/src/main/java/com/project/back_end/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,24 @@
+package com.project.back_end.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class ResourceNotFoundException extends BusinessLogicException {
+    private static final String DEFAULT_MSG = "Resource not found";
+    private static final HttpStatus STATUS = HttpStatus.NOT_FOUND;
+    
+    public ResourceNotFoundException() {
+        super(DEFAULT_MSG, STATUS);
+    }
+    
+    public ResourceNotFoundException(String message) {
+        super(message != null && message.isBlank() ? message : DEFAULT_MSG, STATUS);
+    }
+    
+    public ResourceNotFoundException(Throwable cause) {
+        super(DEFAULT_MSG, STATUS, cause);
+    }
+    
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message != null && message.isBlank() ? message : DEFAULT_MSG, STATUS, cause);
+    }
+}
