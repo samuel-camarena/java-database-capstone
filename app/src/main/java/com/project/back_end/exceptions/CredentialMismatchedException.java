5096befd071cc7eecda761a7e1053ddabce95824
samuel <samuelcamarenacaballer@gmail.com>
2026-04-29 16:16:27 +0200

    feat(exception): add full implementation for credential mismatch between resource's ID and extracted JWT token subject

---
+++ app/src/main/java/com/project/back_end/exceptions/CredentialMismatchedException.java
@@ -0,0 +1,25 @@
+package com.project.back_end.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class CredentialMismatchedException extends BusinessLogicException {
+    private static final String DEFAULT_MSG = "Credential mismatched";
+    private static final HttpStatus STATUS = HttpStatus.UNAUTHORIZED;
+    
+    public CredentialMismatchedException() {
+        super(DEFAULT_MSG, STATUS);
+    }
+    
+    public CredentialMismatchedException(String message) {
+        super(message != null && message.isBlank() ? message : DEFAULT_MSG, STATUS);
+    }
+    
+    public CredentialMismatchedException(Throwable cause) {
+        super(DEFAULT_MSG, STATUS, cause);
+    }
+    
+    public CredentialMismatchedException(String message, Throwable cause) {
+        super(message != null && message.isBlank() ? message : DEFAULT_MSG, STATUS, cause);
+    }
+    
+}
