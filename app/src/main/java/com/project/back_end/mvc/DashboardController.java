d258b193fbb420b47dea8255aea7da7044fa09c7
samuel <samuelcamarenacaballer@gmail.com>
2026-04-29 21:40:54 +0200

    Revert "feat(service): add full implementation of Main Service"
    This reverts commit b373ccb363a73096383fefa0d0bec881c7ce59c7.

--- app/src/main/java/com/project/back_end/mvc/DashboardController.java
+++ app/src/main/java/com/project/back_end/mvc/DashboardController.java
@@ -1,12 +1,10 @@
 package com.project.back_end.mvc;
 
-import com.project.back_end.services.Service;
+import com.project.back_end.services.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.Map;
 
 /**
  * This class handles routing to admin and doctor dashboard pages based on token validation.
@@ -15,15 +13,15 @@ import java.util.Map;
 public class DashboardController {
     
     @Autowired
-    private Service service;
+    private MainService mainService;
     
     @GetMapping("/adminDashboard/{token}")
     public String adminDashboard(@PathVariable("token") String token) {
 //        try {
-            Map<String, String> response = service.validateToken(token, "admin");
-            if(!response.isEmpty()) {
-                return "redirect:/"; // http://localhost:8080
-            }
+//            Map<String, String> response = MainService.isValidToken(token, "admin");
+//            if(!response.isEmpty()) {
+//                return "redirect:/"; // http://localhost:8080
+//            }
             
             return "redirect:/admin/adminDashboard";
 //        } catch (Exception e) {
@@ -34,10 +32,10 @@ public class DashboardController {
 
     @GetMapping("/doctorDashboard/{token}")
     public String doctorDashboard(@PathVariable("token") String token) {
-        Map<String, String> response = service.validateToken(token, "doctor");
-        if(!response.isEmpty()) {
-            return "redirect:/";
-        }
+//        Map<String, String> response = MainService.isValidToken(token, "doctor");
+//        if(!response.isEmpty()) {
+//            return "redirect:/";
+//        }
         
         return "redirect:/doctor/doctorDashboard";
     }
