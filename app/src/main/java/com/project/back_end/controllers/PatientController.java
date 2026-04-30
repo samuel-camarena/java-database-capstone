b912b2aa626ab9f5f7df888e90707a9ca95dd6d3
samuel <samuelcamarenacaballer@gmail.com>
2026-04-30 16:52:49 +0200

    refactor(controller): medium cleaning method changes and improve happy path reponses

--- app/src/main/java/com/project/back_end/controllers/PatientController.java
+++ app/src/main/java/com/project/back_end/controllers/PatientController.java
@@ -6,11 +6,8 @@ import com.project.back_end.DTO.Login;
 import com.project.back_end.exceptions.EmailOrPhoneAlreadyRegisteredException;
 import com.project.back_end.models.Appointment;
 import com.project.back_end.models.Patient;
-import com.project.back_end.repo.DoctorRepository;
-import com.project.back_end.repo.PatientRepository;
 import com.project.back_end.services.MainService;
 import com.project.back_end.services.PatientService;
-import com.project.back_end.utils.outputhelpers.MessageFormatter.MessageHead;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,41 +22,30 @@ import java.util.Map;
 import static com.project.back_end.utils.AppHelper.composeResponse;
 
 @RestController
-@RequestMapping("/patient") // "${api.path}" + "v1/doctor"
+@RequestMapping("${api.path}" + "v1/patient")
 public class PatientController {
     private static final Logger logger = LoggerFactory.getLogger(PatientController.class);
     private final PatientService patientService;
     private final MainService mainService;
-    private final DoctorRepository doctorRepo;
-    private final PatientRepository patientRepo;
     private final DtoMapper dtoMapper;
     
     @Autowired
-    public PatientController(PatientService patientService, MainService mainService, DoctorRepository doctorRepo, PatientRepository patientRepo, DtoMapper dtoMapper) {
+    public PatientController(PatientService patientService, MainService mainService, DtoMapper dtoMapper) {
         this.patientService = patientService;
         this.mainService = mainService;
-        this.doctorRepo = doctorRepo;
-        this.patientRepo = patientRepo;
         this.dtoMapper = dtoMapper;
     }
     
     /**
      * Handles HTTP GET requests to retrieve patient details using a token.<p>
      * * Validates the token for the `"patient"` role using the shared service.</p>
-     * @param token t
+     * @param token JWT Authorization token
      * @return If the token is valid, returns patient information; otherwise, returns an appropriate error message.
      */
     @GetMapping("/{token}")
-    public ResponseEntity<Map<String, Patient>> getPatient(
-        @PathVariable("Authorization") @Valid String token) {
-        
-        mainService.isValidToken(token, "patient");
-        
-        ResponseEntity<Map<String, Patient>> res = patientService.getPatientDetails(token);
-        Patient patient = res.getBody().get("patient");
-
-        logger.info("{}getPatient::", MessageHead.SUCCESS.compose());
-        return composeResponse(HttpStatus.FOUND, "patient", patient);
+    public ResponseEntity<Map<String, Patient>> getPatient(@PathVariable("Authorization") @Valid String token) {
+        mainService.validateToken(token, "patient");
+        return composeResponse(HttpStatus.OK, "patient", patientService.getPatientDetails(token).get());
     }
     
     /**
@@ -70,12 +56,12 @@ public class PatientController {
      * @return If validation passes, attempts to create the patient and returns success or error messages
      *          based on the outcome.
      */
-    @PostMapping() // "/register"
+    @PostMapping() // "/save"
     public ResponseEntity<Map<String, String>> createPatient(@RequestBody @Valid Patient patient) {
         if (!mainService.isValidPatient(patient))
             throw new EmailOrPhoneAlreadyRegisteredException(
                 "The email " + patient.getEmail() + " or phone " + patient.getPhone() + " is already in use.");
-        patientService.registerPatient(patient);
+        patientService.createPatient(patient);
         return composeResponse(HttpStatus.CREATED, "message", "Patient successfully registered");
     }
 
@@ -94,8 +80,8 @@ public class PatientController {
     
     /**
      * Handles HTTP GET requests to fetch appointment details for a specific patient.<p>
-     * - Requires the patient ID, token, and user role as path variables.<br>
-     * - Validates the token using the shared service.</p>
+     * * Requires the patient ID, token, and user role as path variables.<br>
+     * * Validates the token using the shared service.</p>
      * @param user u
      * @param token t
      * @param id Patient ID
@@ -105,25 +91,11 @@ public class PatientController {
     public ResponseEntity<Map<String, List<AppointmentDTO>>> getPatientAppointment(
         @PathVariable("X-User") @Valid String user,
         @PathVariable("Authorization") @Valid String token,
-        @PathVariable @Valid long id) throws Exception {
-        
-        try {
-            if (mainService.isValidToken(token, user).getStatusCode().isSameCodeAs(HttpStatus.UNAUTHORIZED)) {
-                logger.warn("{}getPatientAppointment::", MessageHead.UNAUTHORIZED.compose());
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-            }
-            Patient patient = patientService.getPatientDetails(token).getBody().get("patient");
-            if (patient == null) {
-                logger.warn("{}getPatientAppointment::", MessageHead.FAIL.compose());
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-            }
+        @PathVariable @Valid long id) {
 
-            logger.info("{}getPatientAppointment::", MessageHead.SUCCESS.compose());
-            return patientService.getPatientAppointment(patient.getId());
-        } catch (Exception e) {
-            logger.error("{}getPatientAppointment:: {}", MessageHead.ERROR.compose(), e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        mainService.validateToken(token, user);
+        List<Appointment> appoints = patientService.getPatientAppointment(id);
+        return composeResponse(HttpStatus.OK, "appointments", dtoMapper.mapAppointmentsToDTOs(appoints));
     }
     
     /**
@@ -136,32 +108,14 @@ public class PatientController {
      * @return If valid, delegates filtering logic to the shared service and returns the filtered result.
      */
     @GetMapping("/appointments/filter")
-    public ResponseEntity<Map<String, List<Appointment>>> filterPatientAppointment(
-        @RequestParam(required = false) String condition,
-        @RequestParam(required = false) String name,
+    public ResponseEntity<Map<String, List<AppointmentDTO>>> filterPatientAppointment(
+        @PathVariable(required = false) String condition,
+        @PathVariable(required = false) String name,
         @PathVariable("Authorization") String token) {
-
-        mainService.isValidToken(token, "patient");
-            
-        ResponseEntity<Map<String, List<AppointmentDTO>>> res = mainService.filterPatient(token, status, name);
-        if (!res.getStatusCode().is2xxSuccessful()) {
-            logger.warn("{}filterPatientAppointment::", MessageHead.FAIL.compose());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
         
-        List<AppointmentDTO> appointsDTO = res.getBody().get("appointments");
-        if (appointsDTO.isEmpty()) {
-            logger.warn("{}filterPatientAppointment::", MessageHead.FAIL.compose());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-        
-        List<Appointment> appoints = appointsDTO
-            .stream()
-            .map(dtoMapper::mapDTOtoAppointment)
-            .toList();
-        
-        logger.info("{}filterPatientAppointment::", MessageHead.SUCCESS.compose());
-        return ResponseEntity.ok(Map.of("appointments", appoints));
+        mainService.validateToken(token, "patient");
+        List<Appointment> appoints = mainService.filterPatient(token, Integer.parseInt(condition), name);
+        return composeResponse(HttpStatus.OK, "appointments", dtoMapper.mapAppointmentsToDTOs(appoints));
     }
 }
 
