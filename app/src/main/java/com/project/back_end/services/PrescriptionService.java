2ca42bd45dfc31775f3fc9fdae8cf68f37ed4d1b
samuel <samuelcamarenacaballer@gmail.com>
2026-04-30 11:11:57 +0200

    refactor(service): change exceptions by logs for fail path as well as improve Javadoc format and info

--- app/src/main/java/com/project/back_end/services/PrescriptionService.java
+++ app/src/main/java/com/project/back_end/services/PrescriptionService.java
@@ -1,9 +1,10 @@
 package com.project.back_end.services;
 
+import com.project.back_end.exceptions.ResourceCreationFailedException;
 import com.project.back_end.exceptions.ResourceNotFoundException;
 import com.project.back_end.models.Prescription;
 import com.project.back_end.repo.PrescriptionRepository;
-import com.project.back_end.utils.outputhelpers.MessageFormatter;
+import com.project.back_end.utils.outputhelpers.MessageFormatter.MessageHead;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,47 +31,39 @@ public class PrescriptionService {
     }
     
     /**
-     * This method saves a new prescription to the database.
-     * - Before saving, it checks if a prescription already exists for the same appointment (using the appointment ID).
-     * - If a prescription exists, it returns a `400 Bad Request` with a message stating the prescription already exists.
-     * - If no prescription exists, it saves the new prescription and returns a `201 Created` status with a success message.
-     * @param prescription p
-     * @return r
+     * This method saves a new prescription to the database.<p>
+     * * Before saving, it checks if a prescription already exists for the same appointment (using the appointment ID).</p>
+     * * If no prescription exists, it saves the new prescription.
+     * * If a prescription exists, it throws a ResourceCreationFailedException with a `400 Bad Request` and
+     * a descriptive message.
+     * @param pres p
      */
     @Transactional
-    public ResponseEntity<Map<String, Integer>> savePrescription(Prescription prescription) {
-        try {
-            if (!prescriptionRepo.findByAppointmentId(prescription.getAppointmentId()).isEmpty()){
-                logger.error("{}savePrescription::", MessageFormatter.MessageHead.FAIL.compose());
-                return composeResponse(FAIL, "status", FAIL.getStatus());
-            }
-            
-            prescriptionRepo.save(prescription);
-            logger.info("{}savePrescription::", MessageFormatter.MessageHead.SUCCESS.compose());
-            return composeResponse(HttpStatus.CREATED, "status", null);
-        } catch (Exception e) {
-            logger.error("{}registerDoctor::", MessageFormatter.MessageHead.ERROR.compose());
-            return composeResponse(SERVER_ERR, "status", SERVER_ERR.getStatus());
-        }
+    public void savePrescription(Prescription pres) {
+        if (!prescriptionRepo.findByAppointmentId(pres.getAppointmentId()).isEmpty())
+            throw new ResourceCreationFailedException("Prescription already associated with appointment ID: "
+                + pres.getAppointmentId());
+        
+        prescriptionRepo.save(pres);
+        logger.info("{}savePrescription:: {}", MessageHead.SUCCESS.compose(),
+            "Prescription successfully saved associated to appointment with ID: " +  pres.getAppointmentId());
     }
     
     /**
-     * Retrieves a prescription associated with a specific appointment based on the `appointmentId`.
-     * * If a prescription is found, it returns it within a map wrapped in a `200 OK` status.
-     * * If there is an error while fetching the prescription, it throws and ResourceNotFoundException`
-     * with HttpStatus code `500 Internal Server Error`, handled by the GlobalExceptionHandler,
-     * logs the error, and adds an error message.
+     * Retrieves a prescription associated with a specific appointment based on the `appointmentId`.<p>
      * @param appointmentId long
      * @return List of Prescriptions found.
      */
     @Transactional
     public List<Prescription> getPrescription(long appointmentId) {
         List<Prescription> prescriptions = prescriptionRepo.findByAppointmentId(appointmentId);
-        if (prescriptions.isEmpty())
-            throw new ResourceNotFoundException("Prescriptions not found for Appointment ID: " + appointmentId);
-        
-        logger.info("{}getPrescription:: {}", MessageFormatter.MessageHead.SUCCESS.compose(),
-            "Prescriptions found for Appointment ID: " + appointmentId);
+        if (prescriptions.isEmpty()) {
+            logger.warn("{}getPrescription:: {}", MessageHead.FAIL.compose(),
+                "Not found Prescriptions for associated appointment with ID: " + appointmentId);
+        } else {
+            logger.info("{}getPrescription:: {}", MessageHead.SUCCESS.compose(),
+                "Found " + prescriptions.size() + " Prescriptions for appointment with ID: " + appointmentId);
+        }
         return prescriptions;
     }
 }
