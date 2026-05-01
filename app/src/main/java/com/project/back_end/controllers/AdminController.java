a11ac48eef723d0f180bcda272ebe0c2460860d5
samuel <samuelcamarenacaballer@gmail.com>
2026-05-01 12:44:13 +0200

    feat(controller): add admin dashboard endpoint

--- app/src/main/java/com/project/back_end/controllers/AdminController.java
+++ app/src/main/java/com/project/back_end/controllers/AdminController.java
@@ -34,5 +34,11 @@ public class AdminController {
         return composeResponse(HttpStatus.OK, "token",
             mainService.validateAdminLogin(admin.getUsername(), admin.getPassword()));
     }
+    
+    @GetMapping("/dashboard/{token}")
+    public String adminDashboard(@PathVariable @Valid String token) {
+        mainService.validateToken(token, "admin");
+        return "admin/adminDashboard";
+    }
 }
 
