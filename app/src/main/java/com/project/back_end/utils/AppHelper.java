fd851f258245cc68aa401a942d0844a85a9aacc5
samuel <samuelcamarenacaballer@gmail.com>
2026-05-14 20:46:15 +0200

    refactor(controller): change minor improvements

--- app/src/main/java/com/project/back_end/utils/AppHelper.java
+++ app/src/main/java/com/project/back_end/utils/AppHelper.java
@@ -1,17 +1,22 @@
 package com.project.back_end.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Map;
 
 public class AppHelper {
+    private static final Logger logger = LoggerFactory.getLogger(AppHelper.class);
     
     public static <T> ResponseEntity<Map<String, T>> composeResponse(
         HttpStatus status,
         String key,
         T payload) throws IllegalArgumentException {
         
+        logger.debug("[composeResponse::] Status: {}, payload: {}", status, payload);
+        //logger.info("[composeResponse::] Status: {}, payload: {}", status, payload);
         return ResponseEntity.status(status).body(Map.of(key, payload));
     }
 }
